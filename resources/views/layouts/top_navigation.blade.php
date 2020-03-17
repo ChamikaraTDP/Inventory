86c65a673dbf0473c630d9bbf649e4f31f229301
ChamikaraTDP <pramudithachamikara@gmail.com>
2020-03-17 11:34:03 +0530

    make this file the entry point to the app add grid content area add js file references

--- resources/views/layouts/top_navigation.blade.php
+++ resources/views/layouts/top_navigation.blade.php
@@ -2,16 +2,21 @@
 
 @section('content')
     <!-- navigation bar -->
-    <div class="nv-box">
+    <div class="nv_box">
         <button onclick="tab_selection(0);">Add</button>
         <button onclick="tab_selection(1);">Issue</button>
         <button onclick="tab_selection(2);">View</button>
         <button onclick="tab_selection(3);">Reports</button>
     </div>
 
-    {{--<div class="grid-container">
-        <div class="grid-box">
-        </div>
-    </div>--}}
-    @yield('forms')
+    {{--@yield('form1')--}}
+
+    <div class="grid_area" id="grid_area"></div>
+
+@endsection
+
+@section('js_area')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    @include('js.inventory_main_js_blade')
+    <script src="{{ asset('js/inventory_main.js') }}" defer></script>
 @endsection
