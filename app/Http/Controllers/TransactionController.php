0102bb7ad6cf617d8a662b5f4a6fec326a8e1c76
ChamikaraTDP <pramudithachamikara@gmail.com>
2020-04-02 08:05:26 +0530

    add inventory items saving functionality

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,12 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Item;
-use App\Station;
 use App\Transaction;
-use App\User;
 use Illuminate\Http\Request;
 use App\Category;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Throwable;
 
@@ -47,30 +44,62 @@ class TransactionController extends Controller
         $data = json_decode($request->input('data'));
 
         $form_data = $data->form_details;
-        $itm_arr = $data->item_details;
+        $itm_bulk = $data->item_details->bulk;
+        $itm_inv = $data->item_details->inv;
+        $bulk_count = count($itm_bulk);
+        $inv_count = count($itm_inv);
 
+        if($bulk_count == 0 && $inv_count == 0){
+            return response(' no items passed! ', 500);
+        }
+
+        $user = auth()->user();
+        $attach_arr = [];
+        $itm_count = 0;
+        $last_id = [];
+        $first_id = 0;
+        $itm_ids = [];
         $transaction = new Transaction;
 
         $transaction->issuing_date = $form_data->date;
         $transaction->receipt_no = $form_data->issue_no;
-        $transaction->received_by = Auth::user()->id;
-        $transaction->receiving_station = Auth::user()->station_id;
+        $transaction->received_by = $user->id;
+        $transaction->receiving_station = $user->station_id;
         $transaction->transaction_type = "to_stock";
         $transaction->supplier = $form_data->supplier;
         $transaction->description = $form_data->description;
         $transaction->save();
 
-        $attach_arr = [];
-
-        $item_count = count($itm_arr);
-        for($i = 0; $i < $item_count; $i++){
-            $attach_arr[$itm_arr[$i]->item_id] = array('quantity' => $itm_arr[$i]->quantity, 'unit_price' => $itm_arr[$i]->unit_price);
+        for($i = 0; $i < $bulk_count; $i++){
+            $attach_arr[$itm_bulk[$i]->item_id] = array('quantity' => $itm_bulk[$i]->quantity,
+                'unit_price' => $itm_bulk[$i]->unit_price);
         }
 
         $transaction->bulk_items()->attach($attach_arr);
 
-        echo "transaction created for add and items attached successfully";
+        $attach_arr = [];
 
+        for($i = 0; $i < $inv_count; $i++){
+            $quan = $itm_inv[$i]->quantity;
+            $itm_count += $quan;
+            for($j = 0; $j < $quan; $j++){
+                array_push($attach_arr,['item_id' => $itm_inv[$i]->item_id,
+                    'price' => $itm_inv[$i]->unit_price]);
+            }
+        }
+
+        DB::table('inventory_items')->insert($attach_arr);
+
+        $last_id = DB::select('select LAST_INSERT_ID() as id');
+        $first_id = $last_id[0]->id;
+
+        for($k = 0; $k < $itm_count; $k++){
+            array_push($itm_ids, ($first_id + $k) );
+        }
+
+        $transaction->inventory_items()->attach($itm_ids);
+
+        return response( " transaction recorded successfully", 200);
     }
 
 
@@ -79,35 +108,36 @@ class TransactionController extends Controller
      * @return array
      */
     private function get_avail_items(){
-        $user = auth()->user()->id;
-
-        $rcv_items = DB::table('users')
-            ->join('transactions', 'users.station_id', '=', 'transactions.receiving_station')
-            ->join('transaction_bulk', 'transactions.id', '=', 'transaction_bulk.transaction_id')
-            ->select('item_id', DB::raw('sum(quantity) as quantity') )
-            ->where('users.id', $user)
-            ->groupBy('item_id')
-            ->get();
-
-        $isu_items = DB::table('users')
-            ->join('transactions', 'users.station_id', '=', 'transactions.issuing_station')
-            ->join('transaction_bulk', 'transactions.id', '=', 'transaction_bulk.transaction_id')
-            ->select('item_id', DB::raw('sum(quantity) as quantity') )
-            ->where('users.id', $user)
-            ->groupBy('item_id')
-            ->get();
-
+        $station = auth()->user()->station_id;
         $avl_items = [];
+
+        $rcv_items = DB::table('transactions')
+            ->join('transaction_bulk', 'transactions.id', '=', 'transaction_bulk.transaction_id')
+            ->join('items', 'transaction_bulk.item_id', '=', 'items.id')
+            ->select('item_id', DB::raw('sum(quantity) as quantity'), 'name', 'category_id' )
+            ->where('transactions.receiving_station', $station)
+            ->groupBy('item_id')
+            ->get();
+
+        $isu_items = DB::table('transactions')
+            ->join('transaction_bulk', 'transactions.id', '=', 'transaction_bulk.transaction_id')
+            ->join('items', 'transaction_bulk.item_id', '=', 'items.id')
+            ->select('item_id', DB::raw('sum(quantity) as quantity'), 'name', 'category_id' )
+            ->where('transactions.issuing_station', $station)
+            ->groupBy('item_id')
+            ->get();
+
         foreach ($rcv_items as $rcv_item) {
             $isu_itm =  $isu_items->firstWhere('item_id', $rcv_item->item_id);
 
             if($isu_itm) {
                 $rcv_item->quantity = $rcv_item->quantity - $isu_itm->quantity;
             }
-            if($rcv_item->quantity > 0){
+            if($rcv_item->quantity > 0) {
                 array_push($avl_items, $rcv_item);
             }
             else if($rcv_item->quantity < 0) {
+
                 return $rcv_item->item_id;
                 break;
             }
@@ -126,22 +156,13 @@ class TransactionController extends Controller
         $avl_items = $this->get_avail_items();
 
         if(is_array($avl_items)) {
-
-            $stations = Station::select('id', 'name')->where('id', '!=', auth()->user()->station_id)->get();
-
-            $users = User::select('id', 'name', 'station_id')->get();
-
-            /*$rcv_items = DB::select('select item_id, sum(quantity) as quantity
-                            from users, transactions, transaction_bulk
-                            where users.id = :user
-                            and users.station_id = transactions.receiving_station
-                            and transactions.id = transaction_bulk.transaction_id group by item_id', ['user' => $user]);*/
-
             try {
                 $issue_view = view('issue_tab')->render();
-                return response()->json(array("issue_view" => $issue_view, "items" => $avl_items,
-                    "stations" => $stations, "users" => $users));
+
+                return response()->json(array("issue_view" => $issue_view, "items" => $avl_items));
+
             } catch (Throwable $e) {
+
                 return response('error occurred during view rendering', 500);
             }
         }
@@ -159,37 +180,31 @@ class TransactionController extends Controller
      * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\JsonResponse|\Illuminate\Http\Response
      */
     public function issue_items(Request $request) {
-        //echo $request;
         $issue = json_decode($request->input('issue'));
 
         $items = $issue->items;
         $details = $issue->details;
+        $attach_arr = [];
+        $item_count = count($items);
+        $avl_items = $this->get_avail_items();
 
         $transaction = new Transaction;
-
         $transaction->issuing_date = $details->isu_date;
-        $transaction->receipt_no = $details->rcp_no;
         $transaction->received_by = $details->rcv_usr;
         $transaction->receiving_station = $details->rcv_stn;
-        $transaction->issue_no = $details->isu_no;
         $transaction->issued_by = auth()->user()->id;
         $transaction->issuing_station = auth()->user()->station_id;
         $transaction->transaction_type = "stn_to_stn";
-
         $transaction->save();
 
-        $attach_arr = [];
-
-        $item_count = count($items);
         for($i = 0; $i < $item_count; $i++){
             $attach_arr[$items[$i]->item_id] = array('quantity' => $items[$i]->quantity);
         }
 
         $transaction->bulk_items()->attach($attach_arr);
 
-        $avl_items = $this->get_avail_items();
-
         if(is_array($avl_items)) {
+
             return response()->json(array("items" => $avl_items,
                 "msg" => "transaction created for issue and items attached successfully"));
         }
