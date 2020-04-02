ab88a076858acfa1f9a32b87530431ff7d082d1c
ChamikaraTDP <pramudithachamikara@gmail.com>
2020-04-02 08:39:11 +0530

    add div for the model window

--- resources/views/add_tab.blade.php
+++ resources/views/add_tab.blade.php
@@ -4,35 +4,35 @@
             <div>
                 <ol>
                     <li>
-                        <label for="form_in_1">Date:</label>
-                        <input id="form_in_1" class="ad_fm_inp" type="date" name="date" required/>
+                        <label for="ad_fm_data">Date:</label>
+                        <input id="ad_fm_date" class="ad_fm_inp" type="date" name="date" required/>
                     </li>
                     <li>
-                        <label for="form_in_2">Received From:</label>
-                        <input id="form_in_2" class="ad_fm_inp" type="text" name="supplier" required/>
+                        <label for="ad_fm_sup">Received From:</label>
+                        <input id="ad_fm_sup" class="ad_fm_inp" type="text" name="supplier" required/>
                     </li>
                     <li>
-                        <label for="form_in_3">Issue Order No:</label>
-                        <input id="form_in_3" class="ad_fm_inp" type="text" name="issue_no" required/>
+                        <label for="ad_fm_isn">Issue Note No:</label>
+                        <input id="ad_fm_isn" class="ad_fm_inp" type="text" name="issue_no" required/>
                     </li>
                 </ol>
             </div>
             <table id="item_tbl">
                 <thead>
-                <tr>
-                    <th>Item Category</th>
-                    <th>Item Name</th>
-                    <th>Quantity</th>
-                    <th>Unit Value(Rs/-)</th>
-                    <th>Action</th>
-                </tr>
+                    <tr>
+                        <th>Item Category</th>
+                        <th>Item Name</th>
+                        <th>Quantity</th>
+                        <th>Unit Value(Rs/-)</th>
+                        <th>Action</th>
+                    </tr>
                 </thead>
                 <tbody id="tbl_body"></tbody>
             </table>
             <div class="ad_btm_area">
                 <div>
-                    <label for="form_in_4">Description:</label>
-                    <input id="form_in_4" type="text" class="ad_fm_inp" name="description"/>
+                    <label for="ad_fm_des">Description:</label>
+                    <input id="ad_fm_des" type="text" class="ad_fm_inp" name="description"/>
                 </div>
                 <div class="ad_sub_btn_dv">
                     <input id="add_sub_btn" type="submit" class="add_btn sub_btn" value="submit">
@@ -42,10 +42,5 @@
     </div>
 </div>
 
-<div id="ad_mdl" class="ad_mdl">
-    <div id="ad_mdl_cont" class="ad_mdl_cont">
-        <span id="ad_mdl_close" class="ad_mdl_close">&times;</span>
-        <p>This is my first model window</p>
-        <button id="mdl_conf_btn" type="button" class="add_btn">Confirm</button>
-    </div>
+<div id="ad_mdl" class="model">
 </div>
