2be965b2e8d65508f92b79957a331018dee0eeae
ChamikaraTDP <pramudithachamikara@gmail.com>
2020-04-02 08:09:52 +0530

    add global helper functions to get the data passed to the blade

--- resources/views/layouts/top_navigation.blade.php
+++ resources/views/layouts/top_navigation.blade.php
@@ -15,8 +15,30 @@
 
 @section('js_area')
     <script>
-        var items = @json($items);
-        var categories = @json($categories);
+        function get_items() {
+            var items = @json($items);
+            return items;
+        }
+
+        function get_categories() {
+            var categories = @json($categories);
+            return categories;
+        }
+
+        function get_stations() {
+            var stations = @json($stations);
+            return stations;
+        }
+
+        function get_users() {
+            var users = @json($users);
+            return users;
+        }
+
+        function get_user() {
+            var user = @json(auth()->user());
+            return user;
+        }
 
         function getCSRF() {
              return '{{csrf_token()}}';
