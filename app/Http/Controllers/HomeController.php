46801977b7417f13f4f1fbb561fcedfba4f2fe42
ChamikaraTDP <pramudithachamikara@gmail.com>
2020-04-02 08:41:27 +0530

    refactor nav_index() to pass station and user data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Category;
 use App\Item;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use mysql_xdevapi\Table;
+use App\Station;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -28,35 +26,17 @@ class HomeController extends Controller
      */
     public function index()
     {
-        //$user = auth()->user()->id;
-
-        /*$avail_items = DB::table('users')
-                           ->join('transactions', 'users.station_id', '=', 'transactions.receiving_station')
-                           ->join('transaction_bulk', 'transactions.id', '=', 'transaction_bulk.transaction_id')
-                           ->select('item_id', DB::raw('sum(quantity) as quantity') )
-                           ->where('users.id', $user)
-                           ->groupBy('item_id')
-                           ->get();*/
-
-        //$test = DB::table('users')->pluck('name');
-
-        //$roles = DB::table('users')->pluck('name', 'id');
-
-        /*foreach ($roles as $id => $name) {
-            echo $name . ' ' . $id;
-        }*/
-
-        //dd($test);
-        //dd($avail_items);
         return view('home');
     }
 
     public function nav_index()
     {
         $categories = Category::select('id', 'name')->get();
-        $items = Item::select('id', 'name', 'category_id')->get();
+        $items = Item::select('id', 'name', 'type', 'category_id')->get();
+        $stations = Station::select('id', 'name')->get();
+        $users = User::select('id', 'name', 'station_id')->get();
 
-        return view('layouts.top_navigation', compact('categories', 'items'));
+        return view('layouts.top_navigation', compact('categories', 'items', 'stations', 'users'));
     }
 
 
