3618dd99922a242437f49a43b4d7109d34772e30
ChamikaraTDP <pramudithachamikara@gmail.com>
2020-05-15 13:36:32 +0530

    design user registration view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,16 +2,101 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="columns is-centered">
+        <div class="column is-6">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <header class="card-header">
+                    <div class="card-header-title is-centered">{{ __('Register') }}</div>
+                </header>
 
-                <div class="card-body">
+                <div class="card-content">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
+                        <div class="field">
+                            <label class="label" for="name">{{ __('Name') }}</label>
 
-                        <div class="form-group row">
+                            <div class="control">
+                                <input id="name" type="text" class="input @error('name') is-danger @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                <span class="help is-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label class="label" for="branch">{{ __('Station') }}</label>
+
+                            <div class="control">
+                                <div class="select">
+                                    <select id="branch" name="branch" required>
+                                        <option value="">Select Station</option>
+
+                                        @foreach(\App\Station::all() as $station)
+                                            <option value="{{ $station->id }}" {{ old('branch') == $station->id ? 'selected' : '' }}>{{ $station->name }}</option>
+                                        @endforeach
+
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label for="username" class="label">{{ __('Username') }}</label>
+
+                            <div class="control">
+                                <input id="username" type="text" class="input @error('username') is-danger @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+
+                                @error('username')
+                                <span class="help is-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label for="password" class="label">{{ __('Password') }}</label>
+
+                            <div class="control">
+                                <input id="password" type="password" class="input @error('password') is-danger @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                <span class="help is-danger" role="alert">
+                                        {{ $message }}
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <label for="password-confirm" class="label">{{ __('Confirm Password') }}</label>
+
+                            <div class="control">
+                                <input id="password-confirm" type="password" class="input" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <div class="control">
+                                <button type="submit" class="button is-link">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+{{--
+<div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
@@ -81,11 +166,4 @@
                                     {{ __('Register') }}
                                 </button>
                             </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+                        </div>--}}
